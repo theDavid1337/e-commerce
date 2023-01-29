3b96fd1ae621a9baf0a8239923fd9153987bf8b1
David <103839294+theDavid1337@users.noreply.github.com>
2023-01-29 22:28:13 +0500

    Updating E-Commerce

---
+++ JS/main.js
@@ -0,0 +1,28 @@
+let header_wrapper = document.querySelector(".header_wrapper"),
+  modal_wrapper_icon = document.querySelectorAll(".modal_wrapper-icon"),
+  search_box = document.querySelector(".search_box"),
+  modal_wrapper = document.querySelector(".modal_wrapper"),
+  menu = document.querySelector(".menu"),
+  toggle_menu = document.querySelector(".toggle_menu");
+
+window.addEventListener("scroll", () => {
+  let window_scroll = window.scrollY;
+
+  if (window_scroll > 45) {
+    header_wrapper.style.top = 0;
+  } else {
+    header_wrapper.style.top = `60px`;
+  }
+});
+
+modal_wrapper_icon.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    search_box.classList.toggle("default");
+    modal_wrapper.classList.toggle("default");
+  });
+});
+
+toggle_menu.addEventListener("click", () => {
+  menu.classList.toggle("default");
+  toggle_menu.classList.toggle("default");
+});
